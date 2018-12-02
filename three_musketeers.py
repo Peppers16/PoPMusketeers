c1a913649ea82d7524f3459806ee7e971a01d7cc
unknown <jhill32@dcs.bbk.ac.uk>
2018-12-02 15:47:19 +0000

    Ammended is_legal_move to return False instead of exception: avoids fatal exception if player selects empty location during play

--- three_musketeers.py
+++ three_musketeers.py
@@ -148,7 +148,7 @@ def is_legal_move(location, direction):
     elif player == 'R':
         return is_legal_move_by_enemy(location, direction)
     else:
-        raise ValueError('Given location does not contain a Musketeer or an Enemy')
+        return False
 
 
 def can_move_piece_at(location):
